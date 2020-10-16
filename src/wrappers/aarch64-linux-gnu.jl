0302c4988452207b6d96507e054919d4f5a1dc63
jlbuild <juliabuildbot@gmail.com>
2020-10-16 18:44:06 +0000

    xkbcommon_jll build 0.9.1+4

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -5,52 +5,15 @@ using Xorg_xkeyboard_config_jll
 using Xorg_libxcb_jll
 using Wayland_jll
 using Wayland_protocols_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libxkbcommon`
-const libxkbcommon_splitpath = ["lib", "libxkbcommon.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libxkbcommon_path = ""
-
-# libxkbcommon-specific global declaration
-# This will be filled out by __init__()
-libxkbcommon_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libxkbcommon = "libxkbcommon.so.0"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("xkbcommon")
+JLLWrappers.@declare_library_product(libxkbcommon, "libxkbcommon.so.0")
 function __init__()
-    global artifact_dir = abspath(artifact"xkbcommon")
+    JLLWrappers.@generate_init_header(Xorg_xkeyboard_config_jll, Xorg_libxcb_jll, Wayland_jll, Wayland_protocols_jll)
+    JLLWrappers.@init_library_product(
+        libxkbcommon,
+        "lib/libxkbcommon.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Xorg_xkeyboard_config_jll.PATH_list, Xorg_libxcb_jll.PATH_list, Wayland_jll.PATH_list, Wayland_protocols_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Xorg_xkeyboard_config_jll.LIBPATH_list, Xorg_libxcb_jll.LIBPATH_list, Wayland_jll.LIBPATH_list, Wayland_protocols_jll.LIBPATH_list,))
-
-    global libxkbcommon_path = normpath(joinpath(artifact_dir, libxkbcommon_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libxkbcommon_handle = dlopen(libxkbcommon_path)
-    push!(LIBPATH_list, dirname(libxkbcommon_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
